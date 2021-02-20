a651b750aa4dcc5282f22b38473174a179a1c177
Andrey-Soares <78767809+Andrey-Soares@users.noreply.github.com>
2021-02-20 17:52:07 -0300

    Add files via upload
    1° Site

---
+++ AniPse/js/anime.js
@@ -0,0 +1,28 @@
+
+// let scrollTop = window.pageYoffset
+
+// window.onscroll = () => {
+
+// 	let scrollTopCurrent = window.pageYOffset
+
+// 	if (scrollTop > scrollTopCurrent) {
+// 		document.querySelector('.main').style.top = "0"
+
+// 	} 
+// 	else{
+// 		document.querySelector('.main').style.top = "-10rem"
+
+// 	}
+
+
+// 	scrollTop = scrollTopCurrent
+
+// }
+
+
+ 	setTimeout(function() {
+     $('#mydiv').hide('fast');
+ }, 1000);
+
+
+
